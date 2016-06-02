e22d5d78f8914ef21f2d8b8d89980663c5405f58
Vish Desai <vish@myndbee.com>
2016-06-02 17:56:39 +0530

    Minor changes

---
+++ knex_migrations/seeds/twyr_server_framework_3.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex, Promise) {
+	var apiGatewayId = null;
+
+	return knex.raw('SELECT id FROM modules WHERE name = ? AND parent_id IS NULL', ['twyr-api-gateway'])
+	.then(function(parentId) {
+		if(!parentId.rows.length)
+			return null;
+
+		apiGatewayId = parentId.rows[0].id;
+		return knex.raw('SELECT id FROM modules WHERE name = ? AND parent_id = ?', ['profile', apiGatewayId]);
+	})
+	.then(function(profileComponentId) {
+		if(profileComponentId.rows.length)
+			return null;
+
+		return knex("modules").insert({ 'parent_id': apiGatewayId, 'type': 'component', 'name': 'profile', 'display_name': 'Profile Manager', 'description': 'The Twy\'r API Gateway User Profile Management Component' });
+	});
+};
